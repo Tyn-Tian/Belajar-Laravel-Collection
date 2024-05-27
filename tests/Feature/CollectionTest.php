a41c5f2f833b209fb29f29e510d96561629fd71c
Christian <kristian2015ok@gmail.com>
2024-05-27 11:28:39 +0700

    Add: Ordering

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -372,4 +372,14 @@ class CollectionTest extends TestCase
             return $value == 8;
         }));
     }
+
+    public function testOrdering()
+    {
+        $collection = collect([1, 3, 2, 4, 5, 7, 6, 8, 9]);
+        $result = $collection->sort();
+        self::assertEqualsCanonicalizing([1, 2, 3, 4, 5, 6, 7, 8, 9], $result->all());
+
+        $result = $collection->sortDesc();
+        self::assertEqualsCanonicalizing([9, 8, 7, 6, 5, 4, 3, 2, 1], $result->all());
+    }
 }
