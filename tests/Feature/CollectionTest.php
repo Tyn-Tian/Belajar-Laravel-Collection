32b2cb68240e5959dfebfe67eb4a6218d43dc756
Christian <kristian2015ok@gmail.com>
2024-05-27 10:03:01 +0700

    Add: Flattening

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -123,4 +123,37 @@ class CollectionTest extends TestCase
             "country" => "Indonesia"
         ], $collection3->all());
     }
+
+    public function testCollapse()
+    {
+        $collection = collect([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]);
+        $result = $collection->collapse();
+        self::assertEquals([
+            1, 2, 3, 4, 5, 6, 7, 8, 9
+        ], $result->all());
+    }
+
+    public function testFlapMap()
+    {
+        $collection = collect([
+            [
+                "name" => "Christian",
+                "hobbies" => ["Coding", "Gaming"]
+            ],
+            [
+                "name" => "Budi",
+                "hobbies" => ["Reading", "Writing"]
+            ],
+        ]);
+        $result = $collection->flatMap(function ($item) {
+            return $item["hobbies"];
+        });
+        self::assertEquals([
+            "Coding", "Gaming", "Reading", "Writing"
+        ], $result->all());
+    }
 }
